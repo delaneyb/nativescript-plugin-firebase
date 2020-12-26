115d576a156ae0d962a57de0256b22c4bf35814c
Ben Delaney <ben@bdelaney.me>
2020-12-27 01:45:55 +1100

    Fix firestore types namespaces confusion
    When defining types of parameters, properties and return types in the nested firebase.firestore namespace, the types on the top level exported firestore namespace actually weren't available/visible. Solutions are to rename the top level firestore namespace as e.g. firestoreNamespace and then use an `export { firestoreNamespace as firestore }`, or to move all those top level exports to a totally separate firestore.d.ts file, and reexport the contents of that, which I have done. I have also done the same for the functions namespace, moving the definitions into src/functions/functions.d.ts

--- src/functions/functions.d.ts
+++ src/functions/functions.d.ts
@@ -1,7 +1,14 @@
-import { functions } from "../firebase";
+export type SupportedRegions =
+    | "us-central1"
+    | "us-east1"
+    | "us-east4"
+    | "europe-west1"
+    | "europe-west2"
+    | "asia-east2"
+    | "asia-northeast1";
 
-export declare type HttpsCallable<I, O> = (callableData: I) => Promise<O>;
+export type HttpsCallable<I, O> = (callableData: I) => Promise<O>;
 
-export declare function httpsCallable<I = {}, O = {}>(functionName: string, region?: functions.SupportedRegions): HttpsCallable<I, O>;
+export declare function httpsCallable<I = {}, O = {}>(functionName: string, region?: SupportedRegions): HttpsCallable<I, O>;
 
 export declare function useFunctionsEmulator(origin: string): void;
