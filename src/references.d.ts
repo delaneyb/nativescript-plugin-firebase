0046666e76c8c834aae49efbcd4033d9e049d5b3
Benjamin Delaney <iottt@me.com>
2021-02-14 01:17:45 +1100

    Update references use types not relative path

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,5 +1,5 @@
-/// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
-/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+/// <reference types="@nativescript/core" />
+/// <reference types="@nativescript/types" />
 
 /// <reference path="./platforms/ios/typings/objc!FBSDKCoreKit.d.ts" />
 /// <reference path="./platforms/ios/typings/objc!FBSDKLoginKit.d.ts" />
